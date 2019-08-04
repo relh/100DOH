62a3d230da0de7e32f8f8c8cfa6d12c058427917
Dandan Shan <dandans@epicfail.eecs.umich.edu>
2019-08-03 21:22:27 -0400

    start

---
+++ temp/scrab.py
@@ -0,0 +1,11 @@
+import urllib.request
+
+
+f = open('./youcook2_home.html', 'w')
+url = r'http://youcook2.eecs.umich.edu/'
+headers ={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+req = urllib.request.Request(url=url,headers=headers)
+res = urllib.request.urlopen(req)
+html = res.read().decode('utf-8')
+print(html)
+f.write(html)
