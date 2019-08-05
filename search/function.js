6d28f28e3f4233fc8e2cd1415bf6a206b9dc04e9
Dandan Shan <dandans@epicfail.eecs.umich.edu>
2019-08-04 20:57:16 -0400

    start update func

--- search/function.js
+++ search/function.js
@@ -1,7 +1,46 @@
 
 
-var openf1 = fso.OpenTextFile("1.txt");
+// var openf1 = fso.OpenTextFile("1.txt");
+// var str1 = openf1.ReadLine();
+// var str2 = openf1.ReadAll();
 
-var str1 = openf1.ReadLine();
+function update(){
+    var choice = document.getElementsByName('genre');
+    if (choice[0].checked){
+        alert(choice[0].value)
+    }
+    else if (choice[1].checked){
+        alert(choice[1].value)
+    }
+    else if (choice[2].checked){
+        alert("diy")
+    }
+    else if (choice[3].checked){
+        alert("diy")
+    }
+    else if (choice[4].checked){
+        alert("diy")
+    }
+    else if (choice[5].checked){
+        alert("diy")
+    }
+    else if (choice[6].checked){
+        alert("diy")
+    }
+    else if (choice[6].checked){
+        alert("diy")
+    }
+    else if (choice[7].checked){
+        alert("diy")
+    }
+    else if (choice[8].checked){
+        alert("diy")
+    }
+    else if (choice[9].checked){
+        alert("diy")
+    }
+    else if (choice[10].checked){
+        alert("diy")
+    }
 
-var str2 = openf1.ReadAll();
+}
